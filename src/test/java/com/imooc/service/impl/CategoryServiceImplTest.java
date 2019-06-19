59224dc24312009bb9b6ebf6ace9dfd3ea02ec88
jackyliping <testmai_9@126.com>
2019-06-19 15:17:13 +0800

    5-2

--- src/test/java/com/imooc/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/imooc/service/impl/CategoryServiceImplTest.java
@@ -19,25 +19,25 @@ public class CategoryServiceImplTest {
     private CategoryServiceImpl categoryService;
 
     @Test
-    public void findOne()  {
+    public void findOne()  throws Exception{
         ProductCategory productCategory = categoryService.findOne(1);
         Assert.assertEquals(new Integer(1), productCategory.getCategoryId());
     }
 
     @Test
-    public void findAll()  {
+    public void findAll() throws Exception {
         List<ProductCategory> productCategoryList = categoryService.findAll();
         Assert.assertNotEquals(0, productCategoryList.size());
     }
 
     @Test
-    public void findByCategoryTypeIn()  {
+    public void findByCategoryTypeIn() throws Exception {
         List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(Arrays.asList(1,2,3,4));
         Assert.assertNotEquals(0, productCategoryList.size());
     }
 
     @Test
-    public void save() {
+    public void save() throws Exception{
         ProductCategory productCategory = new ProductCategory("男生专享", 10);
         ProductCategory result = categoryService.save(productCategory);
         Assert.assertNotNull(result);
