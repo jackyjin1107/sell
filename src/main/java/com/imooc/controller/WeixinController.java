56a1d7cb3d5050425f8b2be3cb0d9c245930d562
jackyliping <testmai_9@126.com>
2019-06-20 10:25:34 +0800

    7-5

---
+++ src/main/java/com/imooc/controller/WeixinController.java
@@ -0,0 +1,25 @@
+package com.imooc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/weixin")
+@Slf4j
+public class WeixinController {
+
+    @GetMapping("/auth")
+    public void auth(@RequestParam("code") String code) {
+        log.info("进入auth方法。。。");
+        log.info("code={}", code);
+
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4e8e375118c97302&secret=e4dda2e57037020805b4e62da0c0903a&code=" + code + "&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
+    }
+}
